8830e767ad703f29b106847cc8ef438df6c0d47d
sandeep <61078034+sandeep123724@users.noreply.github.com>
2026-09-03 20:33:59 +0530

    adding requirements.txt file with necessary dependencies for the project.

--- rag.py
+++ rag.py
@@ -1,1 +1,34 @@
 import faiss
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+knowledge_base =[
+    "High-Level and Readable: Python is designed to be highly readable and has a clean, straightforward syntax that closely resembles plain English, making it exceptionally beginner-friendly.",
+    "Interpreted Language: Python code is executed line by line by an interpreter rather than being compiled into machine code beforehand, which simplifies debugging and rapid prototyping.",
+    "Dynamically Typed: You do not need to explicitly declare the data type of a variable when creating it; Python determines the type automatically at runtime.",
+    "Object-Oriented and Multi-Paradigm: Python supports multiple programming styles, including object-oriented, procedural, and functional programming paradigms.",
+    "Extensive Standard Library: It comes with a vast built-in standard library (\"batteries included\") that provides modules for handling tasks like file I/O, regular expressions, math, and internet protocols.",
+    "Cross-Platform Compatibility: Python code can run seamlessly across various operating systems—including Windows, macOS, Linux, and Unix—without requiring major modifications.",
+    "Massive Ecosystem of Third-Party Packages: The Python Package Index (PyPI) hosts hundreds of thousands of independent packages, expanding its capabilities for web development, data science, automation, and more.",
+    "Dominance in Data Science and AI: Python is the undisputed industry standard for data analysis, machine learning, and artificial intelligence, powered by dominant libraries like NumPy, Pandas, TensorFlow, and PyTorch.",
+    "Automatic Memory Management: Python handles memory allocation and deallocation automatically using reference counting and a built-in garbage collector, freeing developers from manual memory management.",
+    "Strong Community Support: Python boasts one of the largest, most active, and welcoming developer communities in the world, offering endless documentation, tutorials, and open-source contributions.",
+    "Python was created by Guido van Rossum, and its first official release (version 0.9.0) was published in 1991 (with development starting in late 1989)."
+]
+embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+embeddings = embedding_model.encode(knowledge_base)
+embeddings_dimension = embeddings.shape[1]
+embeddings_dimension
+index = faiss.IndexFlatL2(embeddings_dimension)
+index.add(embeddings)
+def rag_query(query,k=2):
+  # We need to convert user's query in embedding
+  query_embedding =  embedding_model.encode([query])
+  distances , indices = index.search(query_embedding,k)
+  retrived_docs = [knowledge_base[i] for i in indices[0]]
+  print(f"Retrived documents (top {k}) : ")
+  for i,doc in enumerate(retrived_docs):
+    print(f"{i+1}. {doc}")
+
+
+
